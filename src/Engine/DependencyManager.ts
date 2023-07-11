ce6dcf516b00010d5da8cc777603114fcb52e8b4
juancho <juancho@juancho.org>
2023-07-11 15:17:25 -0700

    Reworking to make it more focused around a DAG

---
+++ src/Engine/DependencyManager.ts
@@ -0,0 +1,376 @@
+/**
+ *  This class is responsible for managing the dependencies in the sheet
+ * 
+ *  A cell has a property called dependsOn which is a list of cells that the cell depends on
+ * 
+ * This class exports the following functions
+ * 
+ * 0. addCellDependency(cellLabel: string, newDependsOn: string, sheetMemory: SheetMemory): boolean 
+ *    - add a cell dependency to a cell, if the new dependency introduces a circular dependency, then return false
+ * 1. getComputationOrder(sheetMemory: SheetMemory): string[]
+ *   - get the computation order for the sheet this way the FormulaEvaluator can compute the cells in the correct order
+ * 
+ * Internal routines marked private
+ * 
+ * 1. updateDependencies(sheetMemory: SheetMemory): void
+ *  - update the dependencies for all cells in the sheet
+ * 2. updateComputationOrder(sheetMemory: SheetMemory): string[]
+ * - update the computation order for the sheet
+ * 3. expandDependencies(cellLabel: string, sheetMemory: SheetMemory): [boolean, string[]]
+ * - expand the dependencies of a cell recursively 
+ * 
+ */
+
+import SheetMemory from "./SheetMemory";
+import Cell from "./Cell";
+import FormulaBuilder from "./FormulaBuilder";
+import FormulaEvaluator from "./FormulaEvaluator";
+
+
+
+export default class DependencyManager {
+
+
+
+
+    public evaluateSheet(sheetMemory: SheetMemory): void {
+        // update the dependencies in the sheet
+        this.updateDependencies(sheetMemory);
+
+        // compute the computation order for the sheet.
+        let computationOrder = this.updateComputationOrder(sheetMemory);
+
+        // compute the cells in the computation order
+        for (let cellLabel of computationOrder) {
+            let currentCell = sheetMemory.getCellByLabel(cellLabel);
+            let formula = currentCell.getFormula();
+            let calculator = new FormulaEvaluator();
+            let [value, displayString] = calculator.evaluate(formula, sheetMemory);
+            currentCell.setValue(value);
+            currentCell.setDisplayString(displayString);
+            sheetMemory.setCellByLabel(cellLabel, currentCell);
+        }
+    }
+
+
+
+    /**
+     * add a cell dependency to a cell
+     * @param {string} cellLabel - The label of the cell
+     * @param {sheetMemory} SheetMemory - The sheet memory
+     * @returns {void}
+     * 
+     * */
+    public addCellDependency(cellLabel: string, newDependsOn: string, sheetMemory: SheetMemory): void {
+
+        let currentCell = sheetMemory.getCellByLabel(cellLabel);
+        const cachedCellDependsOn: string[] = currentCell.getDependsOn();
+
+
+
+        if (cachedCellDependsOn.includes(newDependsOn)) {
+            return;
+        }
+
+        // add the new dependency to the cell dependsOn to try to expand the dependencies
+        let newCellDependsOn: string[] = [...cachedCellDependsOn, newDependsOn];
+
+        /* set the test dependences for this cell */
+
+        currentCell.setDependsOn(newCellDependsOn);
+        let [isCircular, discoveredDependencies] = this.expandDependencies(cellLabel, sheetMemory);
+
+        // if the cell is circular, then restore the original dependencies and return
+        if (isCircular) {
+            currentCell.setDependsOn(cachedCellDependsOn);
+            return;
+        }
+
+        // lets complete the dependencies for this cell.
+        let expandedDependencies = [...discoveredDependencies];
+
+        /** this cell does not introduce a circular dependency,the cell dependsOn*/
+        currentCell.setDependsOn(expandedDependencies);
+
+        // update the cell in the sheet memory
+        sheetMemory.setCellByLabel(cellLabel, currentCell);
+
+        // update the dependencies of the sheet
+        this.updateDependencies(sheetMemory);
+
+        // update the computation order
+        this.updateComputationOrder(sheetMemory);
+    }
+
+    /**
+     * update the dependencies for all cells in the sheet
+     * @param {sheetMemory} SheetMemory - The sheet memory
+     * @returns {void}
+     * 
+     * */
+    public updateDependencies(sheetMemory: SheetMemory): boolean {
+        for (let column = 0; column < sheetMemory.getMaxColumns(); column++) {
+            for (let row = 0; row < sheetMemory.getMaxRows(); row++) {
+                const cellLabel = Cell.columnRowToCell(column, row);
+
+
+                let currentCell = sheetMemory.getCellByLabel(cellLabel);
+                let currentFormula = currentCell.getFormula();
+
+                // always read the top level depensOn from the formula
+                let currentDependsOn = FormulaBuilder.getCellReferences(currentFormula);
+                currentCell.setDependsOn(currentDependsOn);
+                // if the current cell is in the formula then we are done
+                if (currentDependsOn.includes(cellLabel)) {
+                    return false;
+                }
+
+                sheetMemory.setCellByLabel(cellLabel, currentCell);
+
+
+                // if the cell has no formula, then continue
+                if (currentFormula.length === 0) {
+                    continue;
+                }
+
+                // if the cell has no dependsOn, then continue
+                if (currentDependsOn.length === 0) {
+                    continue;
+                }
+
+                // if we reach here we have top level dependencies that we need to expand.
+                let [isCircular, discoveredDependencies] = this.expandDependencies(cellLabel, sheetMemory);
+
+                // This function will throw an error if a circular dependency is detected
+                // updates to formulas that introduce dependencies that are circular are not allowed
+                // and thus if we find it here we have failed at that task 
+                if (isCircular) {
+                    return false;
+                }
+
+                currentCell.setDependsOn(discoveredDependencies);
+                sheetMemory.setCellByLabel(cellLabel, currentCell);
+
+            }
+        }
+        return true;
+    }
+
+
+
+
+
+
+    /**
+     * recursively expand the dependencies of a cell
+     * @param {string} cellLabel - The label of the cell
+     * @param {sheetMemory} SheetMemory - The sheet memory
+     */
+    /**
+     * 
+     * For any cell with a dependency flush out the complete dependencies.
+     * 
+     * for each cell in the depends on look at the depends on of that cell
+     * and to the list of dependencies for the cell
+     */
+    private expandDependencies(cellLabel: string, sheetMemory: SheetMemory): [boolean, string[]] {
+        let currentCell = sheetMemory.getCellByLabel(cellLabel);
+        let cellDependsOn: string[] = FormulaBuilder.getCellReferences(currentCell.getFormula());
+        let expandedDependencies: string[] = [];
+        let isCircular: boolean = false;
+
+
+
+        /**
+         * if the cell has no dependencies, then return
+         * */
+
+        if (cellDependsOn.length === 0) {
+            return [isCircular, expandedDependencies];
+        }
+
+        /**
+         * if the cell has dependencies, then expand the dependencies
+         * */
+        for (let i = 0; i < cellDependsOn.length; i++) {
+            let currentDependency = cellDependsOn[i];
+
+            let currentDependencyExpandedDependencies = this.expandDependencies(currentDependency, sheetMemory);
+            let currentDependencyIsCircular = currentDependencyExpandedDependencies[0];
+            let currentDependencyExpandedDependenciesList = currentDependencyExpandedDependencies[1];
+
+            if (currentDependencyIsCircular) {
+                isCircular = true;
+                return [isCircular, []];
+            }
+
+
+            /**
+             * if the current dependency is not circular, then add the expanded dependencies to the list of expanded dependencies
+             * */
+            for (let j = 0; j < currentDependencyExpandedDependenciesList.length; j++) {
+                let currentDependencyExpandedDependency = currentDependencyExpandedDependenciesList[j];
+                if (expandedDependencies.indexOf(currentDependencyExpandedDependency) === -1) {
+                    expandedDependencies.push(currentDependencyExpandedDependency);
+                }
+            }
+            // now we add the currentDependency to the list of expanded dependencies
+            if (expandedDependencies.indexOf(currentDependency) === -1) {
+                expandedDependencies.push(currentDependency);
+            }
+        }
+
+        /**
+         * if the cell is already in the list of expanded dependencies, then the cell is circular
+         * */
+        if (expandedDependencies.indexOf(cellLabel) !== -1) {
+
+            isCircular = true;
+            return [isCircular, []];
+
+        }
+
+        return [isCircular, expandedDependencies];
+    }
+
+
+    // does the computation order contain the second array (it does not matter in which order the elements are in)
+    // this is a helper function for updateComputationOrder)
+    private preRequisitesSatisfied(currentComputationSet: Set<string>, cellDependsOn: string[]): boolean {
+        for (const cell of cellDependsOn) {
+            if (currentComputationSet.has(cell) === false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * get the computation order for the sheet
+     * @param {sheetMemory} SheetMemory - The sheet memory
+     * @returns {string[]} - The computation order
+     * 
+     * The algorithm here is to use all of the dependency chains that are contained in the Cells
+     * The cells are partitioned into independent cells and dependent cells
+     * The independent cells are the cells that have no dependencies
+     * The dependent cells are the cells that have dependencies
+     * 
+     * The algorithm adds the independent cells to the resultingComputationOrder
+     * It then looks at the first cell in the cellsToBeProcessed list and if all of the dependencies 
+     * of that cell are in the resultingComputationOrder, then it adds the cell to the resultingComputationOrder at the end
+     * 
+     * If the cell cannot be computed at this point it is then put back in the cells to be Processed.
+     * 
+     * There is a problem with this algorithm.  It does not detect circular dependencies.
+     * therefore we are assuming that there are no circular dependencies.  The circular dependencies will be detected
+     * when an attempt to add a cell reference to the formula of a cell is made.  
+     * 
+     * */
+    public updateComputationOrder(sheetMemory: SheetMemory): string[] {
+        let resultingComputationOrder: string[] = [];
+        let independentCells: string[] = [];
+        let cellsToBeProcessed: string[] = [];
+
+
+        // first split the cells into independent and dependent cells (cellsToBeProcessed)
+        for (let column = 0; column < sheetMemory.getMaxColumns(); column++) {
+            for (let row = 0; row < sheetMemory.getMaxRows(); row++) {
+                let currentLabel = Cell.columnRowToCell(column, row);
+                const currentCell = sheetMemory.getCellByLabel(currentLabel);
+
+                if (currentCell.getDependsOn().length === 0) {
+                    independentCells.push(currentLabel);
+                }
+                else {
+                    cellsToBeProcessed.push(currentLabel);
+                }
+            }
+        }
+
+        // now add the independent cells to the computation order
+        resultingComputationOrder = [...independentCells];
+
+        // now add the dependent cells to the computation order
+        while (cellsToBeProcessed.length > 0) {
+            let currentCell = cellsToBeProcessed.shift();
+            if (currentCell === undefined) {
+                throw new Error("currentCell is undefined");
+            }
+            let currentCellDependsOn = sheetMemory.getCellByLabel(currentCell).getDependsOn();
+            let currentComputationSet = new Set(resultingComputationOrder);
+
+            // if the currentCellDepends on is a subset of the currentComputationSet, then add the currentCell to 
+            //the computation order after all the ones that are already there
+            // otherwise put it at the end of the cellToBeProcessed list
+
+            if (this.preRequisitesSatisfied(currentComputationSet, currentCellDependsOn)) {
+                resultingComputationOrder.push(currentCell);
+            }
+            else {
+                cellsToBeProcessed.push(currentCell);
+            }
+        }
+
+        return resultingComputationOrder;
+    }
+
+
+
+    public updateComputationOrderBad(sheetMemory: SheetMemory): string[] {
+        let newComputationOrder: string[] = [];
+        let independentCells: string[] = [];
+
+        let maxRows: number = sheetMemory.getMaxRows();
+        let maxColumns: number = sheetMemory.getMaxColumns();
+        for (let row = 0; row < maxRows; row++) {
+            for (let column = 0; column < maxColumns; column++) {
+
+                let currentLabel = Cell.columnRowToCell(column, row);
+                const currentCell = sheetMemory.getCellByLabel(currentLabel);
+
+                /**
+                 * if the cell has no dependencies, then it is an independent cell
+                 */
+                let currentDependsOn = currentCell.getDependsOn();
+
+                if (currentDependsOn.length === 0) {
+                    independentCells.push(currentLabel);
+                }
+                /**
+                 * if the cell has dependencies, then it is a dependent cell
+                 * 
+                 * add the cell to the computation order after the cells that it depends on
+                 */
+                else {
+                    for (let i = 0; i < currentDependsOn.length; i++) {
+                        let currentDependsOnLabel = currentDependsOn[i];
+                        let currentDependsOnIndex = newComputationOrder.indexOf(currentDependsOnLabel);
+                        if (currentDependsOnIndex === -1) {
+                            newComputationOrder.push(currentDependsOnLabel);
+                        }
+                    }
+                    newComputationOrder.push(currentLabel);
+                }
+
+            }
+        }
+        /**
+         * add the independent cells to the computation order
+         *  
+         * */
+        for (let i = 0; i < independentCells.length; i++) {
+            let currentLabel = independentCells[i];
+            let currentLabelIndex = newComputationOrder.indexOf(currentLabel);
+            if (currentLabelIndex === -1) {
+                newComputationOrder.push(currentLabel);
+            }
+        }
+        return newComputationOrder;
+    }
+
+
+}
+
+
+
+
---
+++ src/Tests/Unit/DependencyManager.test.ts
@@ -0,0 +1,350 @@
+/**
+ * RecalcDependency.ts
+ */
+import DependencyManager from "../../Engine/DependencyManager";
+import SheetMemory from "../../Engine/SheetMemory";
+import Cell from "../../Engine/Cell";
+import { get } from "http";
+
+let testMemory: SheetMemory;
+let dependencyManager: DependencyManager;
+beforeEach(() => {
+  testMemory = new SheetMemory(3, 3);
+  dependencyManager = new DependencyManager();
+
+  const cellA1 = new Cell();
+  cellA1.setFormula(["A2"]);
+  cellA1.setValue(1);
+  cellA1.setDisplayString("1");
+  cellA1.setDependsOn(["A2"]);
+
+  testMemory.setCurrentCellCoordinates(0, 0);
+  testMemory.setCurrentCell(cellA1);
+
+
+  const cellA2 = new Cell();
+  cellA2.setFormula(["2"]);
+  cellA2.setValue(2);
+  cellA2.setDisplayString("2");
+  cellA2.setDependsOn([]);
+  testMemory.setCurrentCellCoordinates(0, 1);
+  testMemory.setCurrentCell(cellA2);
+
+  const cellA3 = new Cell();
+  cellA3.setFormula(["A1"]);
+  cellA3.setValue(3);
+  cellA3.setDisplayString("3");
+  cellA3.setDependsOn(["A1", "A2"]);
+  testMemory.setCurrentCellCoordinates(0, 2);
+  testMemory.setCurrentCell(cellA3);
+}
+);
+
+describe("RecalcDependency", () => {
+
+  describe("WORKING updateComputationOrder", () => {
+    it("should update the computationOrder to be in the correct order", () => {
+
+      const computationOrder = dependencyManager.updateComputationOrder(testMemory);
+      let lastCell = computationOrder[computationOrder.length - 1];
+      let penultimateCell = computationOrder[computationOrder.length - 2];
+
+      expect(lastCell).toEqual("A3");
+      expect(penultimateCell).toEqual("A1");
+    });
+  });
+
+
+
+
+  describe("add two Dependency to cell", () => {
+    it("should add the new dependency to the cell", () => {
+      let testMemory: SheetMemory = new SheetMemory(3, 3);
+      let testWriteCell: Cell = new Cell();
+      testWriteCell.setFormula(["A2", "-", "A3"]);
+      testWriteCell.setValue(0);
+      testWriteCell.setDisplayString("0");
+      testMemory.setCurrentCellCoordinates(0, 0);
+      testMemory.setCurrentCell(testWriteCell);
+
+      const cellB1 = new Cell();
+      cellB1.setFormula(["A1"]);
+      testMemory.setCurrentCellCoordinates(1, 0);
+      testMemory.setCurrentCell(cellB1);
+
+      dependencyManager.updateDependencies(testMemory);
+      const B1DependsOn = testMemory.getCellByLabel("B1").getDependsOn();
+
+      const foundFirstDependency = (B1DependsOn[0] === "A2" || B1DependsOn[0] === "A3");
+      const foundSecondDependency = (B1DependsOn[1] === "A2" || B1DependsOn[1] === "A3") && B1DependsOn[1] !== B1DependsOn[0];
+      const foundThirdDependency = B1DependsOn[2] === "A1";
+      const satisfiedTest = foundFirstDependency && foundSecondDependency && foundThirdDependency;
+      expect(satisfiedTest).toEqual(true);
+      expect(B1DependsOn[2]).toEqual("A1");
+
+    }
+    );
+  });
+
+  describe("REVERSE add a chain A1 depends on A2, A2 depends on A3, A3 depends on A4", () => {
+    it("should add the new dependency to the cell", () => {
+      let testMemory: SheetMemory = new SheetMemory(5, 5);
+      let A1Cell = new Cell();
+      A1Cell.setFormula(["A2"]);
+      A1Cell.setValue(0);
+      A1Cell.setDisplayString("0");
+
+      testMemory.setCurrentCellCoordinates(0, 0);
+      testMemory.setCurrentCell(A1Cell);
+
+      let A2Cell = new Cell();
+      A2Cell.setFormula(["A3"]);
+      A2Cell.setValue(0);
+      A2Cell.setDisplayString("0");
+
+      testMemory.setCurrentCellCoordinates(0, 1);
+      testMemory.setCurrentCell(A2Cell);
+
+      let A3Cell = new Cell();
+      A3Cell.setFormula(["A4"]);
+      A3Cell.setValue(0);
+      A3Cell.setDisplayString("0");
+
+      testMemory.setCurrentCellCoordinates(0, 2);
+      testMemory.setCurrentCell(A3Cell);
+
+      let A4Cell = new Cell();
+      A4Cell.setFormula(["4"]);
+      A4Cell.setValue(4);
+      A4Cell.setDisplayString("4");
+
+      testMemory.setCurrentCellCoordinates(0, 3);
+      testMemory.setCurrentCell(A4Cell);
+      dependencyManager.updateDependencies(testMemory);
+
+      expect(testMemory.getCellByLabel("A1").getDependsOn()).toEqual(["A4", "A3", "A2"]);
+      expect(testMemory.getCellByLabel("A2").getDependsOn()).toEqual(["A4", "A3"]);
+      expect(testMemory.getCellByLabel("A3").getDependsOn()).toEqual(["A4"]);
+    }
+    );
+  });
+
+  describe("FORWARD add a chain A1 depends on A2, A2 depends on B1, B1 depends on B2", () => {
+    it("should add the new dependency to the cell", () => {
+      let testMemoryInt: SheetMemory = new SheetMemory(2, 2);
+      let A1Cell = new Cell();
+      A1Cell.setFormula(["A2"]);
+      A1Cell.setValue(0);
+      A1Cell.setDisplayString("0");
+
+
+      testMemoryInt.setCurrentCellCoordinates(0, 0);
+      testMemoryInt.setCurrentCell(A1Cell);
+
+      let A2Cell = new Cell();
+      A2Cell.setFormula(["B1"]);
+      A2Cell.setValue(0);
+      A2Cell.setDisplayString("0");
+
+
+      testMemoryInt.setCurrentCellCoordinates(0, 1);
+      testMemoryInt.setCurrentCell(A2Cell);
+
+      let B1Cell = new Cell();
+      B1Cell.setFormula(["B2"]);
+      B1Cell.setValue(0);
+      B1Cell.setDisplayString("0");
+
+
+      testMemoryInt.setCurrentCellCoordinates(1, 0);
+      testMemoryInt.setCurrentCell(B1Cell);
+
+      let B2Cell = new Cell();
+      B2Cell.setFormula(["2"]);
+      B2Cell.setValue(2);
+      B2Cell.setDisplayString("2");
+
+
+      dependencyManager.updateDependencies(testMemoryInt);
+
+
+
+      expect(testMemoryInt.getCellByLabel("A1").getDependsOn()).toEqual(["B2", "B1", "A2"]);
+      expect(testMemoryInt.getCellByLabel("A2").getDependsOn()).toEqual(["B2", "B1"]);
+      expect(testMemoryInt.getCellByLabel("B1").getDependsOn()).toEqual(["B2"]);
+
+    });
+  });
+  describe("A 3 by 3 sheet with the first cell being a sum of all the other cells", () => {
+    it("should result in a dependsOn array of all the other cells", () => {
+      let testMemory: SheetMemory = new SheetMemory(3, 3);
+      let cellA1: Cell = new Cell();
+      cellA1.setFormula(["A2", "+", "A3", "+", "B1", "+", "B2", "+", "B3", "+", "C1", "+", "C2", "+", "C3"]);
+      cellA1.setValue(0);
+      cellA1.setDisplayString("0");
+      testMemory.setCurrentCellCoordinates(0, 0);
+      testMemory.setCurrentCell(cellA1);
+
+
+
+      dependencyManager.updateDependencies(testMemory);
+      const A1DependsOn = testMemory.getCellByLabel("A1").getDependsOn();
+      let A1DependsOnSet = new Set(A1DependsOn);
+      let expectedSet = new Set(["A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]);
+      expect(A1DependsOnSet).toEqual(expectedSet);
+    });
+  });
+
+  describe("A 3 by 3 sheet with all the cells (exept for the first one) to contain the formula A1", () => {
+    it("should result in each other cell having A1 in their dependsOn array", () => {
+      let testMemory: SheetMemory = new SheetMemory(3, 3);
+      let cellA1: Cell = new Cell();
+      cellA1.setFormula([]);
+      cellA1.setValue(0);
+      cellA1.setDisplayString("0");
+      testMemory.setCurrentCellCoordinates(0, 0);
+      testMemory.setCurrentCell(cellA1);
+
+      let cellOther: Cell = new Cell();
+      cellOther.setFormula(["A1"]);
+      cellOther.setValue(0);
+      cellOther.setDisplayString("0");
+
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (i === 0 && j === 0) {
+            continue;
+          }
+          testMemory.setCurrentCellCoordinates(i, j);
+          testMemory.setCurrentCell(cellOther);
+        }
+      }
+      dependencyManager.updateDependencies(testMemory);
+
+      const A1DependsOn = testMemory.getCellByLabel("A1").getDependsOn();
+      expect(A1DependsOn).toEqual([]);
+
+      const A2DependsOn = testMemory.getCellByLabel("A2").getDependsOn();
+      expect(A2DependsOn).toEqual(["A1"]);
+
+      const A3DependsOn = testMemory.getCellByLabel("A3").getDependsOn();
+      expect(A3DependsOn).toEqual(["A1"]);
+
+      const B1DependsOn = testMemory.getCellByLabel("B1").getDependsOn();
+      expect(B1DependsOn).toEqual(["A1"]);
+
+      const B2DependsOn = testMemory.getCellByLabel("B2").getDependsOn();
+      expect(B2DependsOn).toEqual(["A1"]);
+
+      const B3DependsOn = testMemory.getCellByLabel("B3").getDependsOn();
+      expect(B3DependsOn).toEqual(["A1"]);
+
+      const C1DependsOn = testMemory.getCellByLabel("C1").getDependsOn();
+      expect(C1DependsOn).toEqual(["A1"]);
+
+      const C2DependsOn = testMemory.getCellByLabel("C2").getDependsOn();
+      expect(C2DependsOn).toEqual(["A1"]);
+
+      const C3DependsOn = testMemory.getCellByLabel("C3").getDependsOn();
+      expect(C3DependsOn).toEqual(["A1"]);
+
+    });
+
+    describe("A 3 by 3 sheet with all of the cells being in a chain in reverse order", () => {
+      let testMemory: SheetMemory = new SheetMemory(3, 3);
+      dependencyManager = new DependencyManager();
+      let cellC3: Cell = new Cell();
+      cellC3.setFormula(["C2", "+", "1"]);
+      testMemory.setCurrentCellLabel("C3");
+      testMemory.setCurrentCell(cellC3);
+
+      let cellC2: Cell = new Cell();
+      cellC2.setFormula(["C1", "+", "1"]);
+      testMemory.setCurrentCellLabel("C2");
+      testMemory.setCurrentCell(cellC2);
+
+      let cellC1: Cell = new Cell();
+      cellC1.setFormula(["B3", "+", "1"]);
+      testMemory.setCurrentCellLabel("C1");
+      testMemory.setCurrentCell(cellC1);
+
+
+      let cellB3: Cell = new Cell();
+      cellB3.setFormula(["B2", "+", "1"]);
+      testMemory.setCurrentCellLabel("B3");
+      testMemory.setCurrentCell(cellB3);
+
+      let cellB2: Cell = new Cell();
+      cellB2.setFormula(["B1", "+", "1"]);
+      testMemory.setCurrentCellLabel("B2");
+      testMemory.setCurrentCell(cellB2);
+
+      let cellB1: Cell = new Cell();
+      cellB1.setFormula(["A3", "+", "1"]);
+      testMemory.setCurrentCellLabel("B1");
+      testMemory.setCurrentCell(cellB1);
+
+      let cellA3: Cell = new Cell();
+      cellA3.setFormula(["A2", "+", "1"]);
+      testMemory.setCurrentCellLabel("A3");
+      testMemory.setCurrentCell(cellA3);
+
+      let cellA2: Cell = new Cell();
+      cellA2.setFormula(["A1", "+", "1"]);
+      testMemory.setCurrentCellLabel("A2");
+      testMemory.setCurrentCell(cellA2);
+
+      let cellA1: Cell = new Cell();
+      cellA1.setFormula(["45"]);
+      testMemory.setCurrentCellLabel("A1");
+      testMemory.setCurrentCell(cellA1);
+
+      dependencyManager.updateDependencies(testMemory);
+      dependencyManager.updateComputationOrder(testMemory);
+
+      let C3DependsOn = new Set(testMemory.getCellByLabel("C3").getDependsOn());
+      let expectedSet = new Set(["C2", "C1", "B3", "B2", "B1", "A3", "A2", "A1"]);
+      expect(C3DependsOn).toEqual(expectedSet);
+
+      let C2DependsOn = new Set(testMemory.getCellByLabel("C2").getDependsOn());
+      expectedSet = new Set(["C1", "B3", "B2", "B1", "A3", "A2", "A1"]);
+      expect(C2DependsOn).toEqual(expectedSet);
+
+      let C1DependsOn = new Set(testMemory.getCellByLabel("C1").getDependsOn());
+      expectedSet = new Set(["B3", "B2", "B1", "A3", "A2", "A1"]);
+      expect(C1DependsOn).toEqual(expectedSet);
+
+      let B3DependsOn = new Set(testMemory.getCellByLabel("B3").getDependsOn());
+      expectedSet = new Set(["B2", "B1", "A3", "A2", "A1"]);
+      expect(B3DependsOn).toEqual(expectedSet);
+
+      let B2DependsOn = new Set(testMemory.getCellByLabel("B2").getDependsOn());
+      expectedSet = new Set(["B1", "A3", "A2", "A1"]);
+      expect(B2DependsOn).toEqual(expectedSet);
+
+      let B1DependsOn = new Set(testMemory.getCellByLabel("B1").getDependsOn());
+      expectedSet = new Set(["A3", "A2", "A1"]);
+      expect(B1DependsOn).toEqual(expectedSet);
+
+      let A3DependsOn = new Set(testMemory.getCellByLabel("A3").getDependsOn());
+      expectedSet = new Set(["A2", "A1"]);
+      expect(A3DependsOn).toEqual(expectedSet);
+
+      let A2DependsOn = new Set(testMemory.getCellByLabel("A2").getDependsOn());
+      expectedSet = new Set(["A1"]);
+      expect(A2DependsOn).toEqual(expectedSet);
+
+
+
+      let expectedComputation = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"];
+      let actualComputation = dependencyManager.updateComputationOrder(testMemory);
+      expect(actualComputation).toEqual(expectedComputation);
+
+    });
+
+
+  });
+
+
+});
+
