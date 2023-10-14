255bff3df117bad4238c7c669eb9ab659d793b4a
sillyfunnypedro <sillyfunnypedro@gmail.com>
2023-10-14 11:19:30 -0700

    added prefix and post fix to the evaluator

---
+++ src/Engine/NewFormulaEvaluator.ts
@@ -0,0 +1,289 @@
+/** A recursive descent parser to deal with the following grammar
+ * 
+ * binary operators +, -, *, /
+ * unary operator -
+ * 
+ * postfix operators % +/- sin cos tan asin acos atan sqrt pow sqr
+ * 
+ * formula = expression
+ * expression = term { ("+" | "-") term }
+ * term = factor { ("*" | "/") factor }
+ * factor = number | "(" expression ")" | unary_op factor | factor postfix_op   
+ * unary_op = "-"
+ * postfix_op = % | +/- | sin | cos | tan | asin | acos | atan | sqrt | pow | sqr
+ * 
+ * 
+ * 
+ */
+
+import Cell from "./Cell"
+import SheetMemory from "./SheetMemory"
+import { ErrorMessages } from "./GlobalDefinitions";
+
+class FormulaEvaluator {
+    private _errorOccured: boolean = false;
+    private _errorMessage: string = "";
+    private _currentFormula: FormulaType = [];
+    private _lastResult: number = 0;
+    private _sheetMemory: SheetMemory;
+    private _result: number = 0;
+    private _postfixOperators = ["+/-", "sin", "cos", "tan", "asin", "acos", "atan", "sqrt", "sqr", "cube", "1/x", "cuberoot"]
+
+    constructor(memory: SheetMemory) {
+        this._sheetMemory = memory;
+    }
+
+    evaluate(formula: FormulaType) {
+        this._errorOccured = false;
+        // make a copy of the formula
+        //
+        // set the currentFormula to the copy of the formula
+        // we do this because the parser consumes the value in currentFormula 
+        this._currentFormula = [...formula];
+
+        if (this._currentFormula.length === 0) {
+            this._result = 0;
+
+            this._errorMessage = ErrorMessages.emptyFormula;
+        }
+
+        this._errorMessage = "";
+        let resultValue = this.expression();
+        this._result = resultValue;
+
+        // if there are still tokens in the formula set the errorOccured flag
+        // if an error has occured then we dont update the error message
+        if (this._currentFormula.length > 0 && !this._errorOccured) {
+            this._errorOccured = true;
+            this._errorMessage = ErrorMessages.invalidFormula;
+        }
+
+        // if an error occured  and the message is PARTIAL return the last result
+        if (this._errorOccured) {
+            this._result = this._lastResult;
+        }
+    }
+    public get error(): string {
+        return this._errorMessage
+    }
+
+    public get result(): number {
+        return this._result;
+    }
+
+    /**
+     * 
+     * @returns The value of the factor in the tokenized formula
+     */
+    private expression(): number {
+        let result = this.term();
+        while (this._currentFormula.length > 0 && (this._currentFormula[0] === "+" || this._currentFormula[0] === "-")) {
+            let operator = this._currentFormula.shift();
+            if (operator === "+") {
+                result += this.term();
+            } else {
+                result -= this.term();
+            }
+        }
+        this._lastResult = result;
+        return result;
+    }
+
+    /**
+     * 
+     * @returns The value of the term in the tokenized formula
+     */
+    private term(): number {
+        let result = this.factor();
+        while (this._currentFormula.length > 0 && (this._currentFormula[0] === "*" || this._currentFormula[0] === "/")) {
+            let operator = this._currentFormula.shift();
+            let factor = this.factor();
+            if (operator === "*") {
+                result *= factor;
+            } else {
+                // check for divide by zero
+                if (factor === 0) {
+                    this._errorOccured = true;
+                    this._errorMessage = ErrorMessages.divideByZero;
+                    this._lastResult = Infinity;
+                    return Infinity;
+                }
+                // we are ok, lets divide
+                result /= factor;
+            }
+        }
+        this._lastResult = result;
+        return result;
+    }
+
+    /**
+   *  
+   * @returns The value of the factor in the tokenized formula
+   * 
+   */
+    private factor(): number {
+        let result = 0;
+        // if the formula is empty set errorOccured to true 
+        // and set the errorMessage to "PARTIAL"
+        // and return 0
+        if (this._currentFormula.length === 0) {
+            this._errorOccured = true;
+            this._errorMessage = ErrorMessages.partial;
+            return result;
+        }
+
+        // get the first token in the formula
+        let token = this._currentFormula.shift();
+
+        // if the token is a number set the result to the number
+        // and set the lastResult to the number
+
+
+        if (this.isNumber(token)) {
+            result = Number(token);
+            this._lastResult = result;
+
+            // if the token is a "(" get the value of the expression
+        } else if (token === "(") {
+            result = this.expression();
+            if (this._currentFormula.length === 0 || this._currentFormula.shift() !== ")") {
+                this._errorOccured = true;
+                this._errorMessage = ErrorMessages.missingParentheses;
+                this._lastResult = result
+            }
+
+            // if the token is a cell reference get the value of the cell
+        } else if (this.isCellReference(token)) {
+            [result, this._errorMessage] = this.getCellValue(token);
+
+            // if the cell value is a number set the result to the number
+            if (this._errorMessage !== "") {
+                this._errorOccured = true;
+                this._lastResult = result;
+            }
+
+        } else if (token === "rand") {
+            result = Math.random();
+        } else if (token === "-") {
+            result = -this.factor();
+        } else if (token === "+") {
+            result = this.factor();
+        } else {
+            this._errorOccured = true;
+            this._errorMessage = ErrorMessages.invalidFormula;
+        }
+
+        result = this.postfix(result);
+        return result;
+    }
+
+    /**
+     * 
+     * @param token 
+     * @returns The value of the postfix operation
+     */
+    private postfix(currentValue: number): number {
+        let result = currentValue;
+
+        if (this._currentFormula.length === 0) {
+            return result;
+        }
+
+        let tokenPeek = this._currentFormula[0];
+
+        if (this._postfixOperators.includes(tokenPeek)) {
+            let token = this._currentFormula.shift();
+            result = this.postfixOperation(token, result);
+        }
+        return result;
+    }
+
+    private postfixOperation(token: TokenType, currentValue: number): number {
+
+        let result = currentValue;
+        if (token === "+/-") {
+            result = -currentValue;
+        } else if (token === "sin") {
+            result = Math.sin(currentValue);
+        } else if (token === "cos") {
+            result = Math.cos(currentValue);
+        } else if (token === "tan") {
+            result = Math.tan(currentValue);
+        } else if (token === "asin") {
+            result = Math.asin(currentValue);
+        } else if (token === "acos") {
+            result = Math.acos(currentValue);
+        } else if (token === "atan") {
+            result = Math.atan(currentValue);
+        } else if (token === "sqrt") {
+            result = Math.sqrt(currentValue);
+        } else if (token === "cuberoot") {
+            result = Math.pow(currentValue, 1.0 / 3.0);
+        } else if (token === "sqr") {
+            result = Math.pow(currentValue, 2);
+        } else if (token === "cube") {
+            result = Math.pow(currentValue, 3);
+        } else if (token === "1/x") {
+            if (currentValue === 0) {
+                this._errorOccured = true;
+                this._errorMessage = ErrorMessages.divideByZero;
+                this._lastResult = Infinity;
+                return Infinity;
+            }
+            result = 1.0 / currentValue;
+        }
+        return result;
+    }
+
+    /**
+   * 
+   * @param token 
+   * @returns true if the toke can be parsed to a number
+   */
+    isNumber(token: TokenType): boolean {
+        return !isNaN(Number(token));
+    }
+
+    /**
+     * 
+     * @param token
+     * @returns true if the token is a cell reference
+     * 
+     */
+    isCellReference(token: TokenType): boolean {
+
+        return Cell.isValidCellLabel(token);
+    }
+
+    /**
+     * 
+     * @param token
+     * @returns [value, ""] if the cell formula is not empty and has no error
+     * @returns [0, error] if the cell has an error
+     * @returns [0, ErrorMessages.invalidCell] if the cell formula is empty
+     * 
+     */
+    getCellValue(token: TokenType): [number, string] {
+
+        let cell = this._sheetMemory.getCellByLabel(token);
+        let formula = cell.getFormula();
+        let error = cell.getError();
+
+        // if the cell has an error return 0
+        if (error !== "" && error !== ErrorMessages.emptyFormula) {
+            return [0, error];
+        }
+
+        // if the cell formula is empty return 0
+        if (formula.length === 0) {
+            return [0, ErrorMessages.invalidCell];
+        }
+
+
+        let value = cell.getValue();
+        return [value, ""];
+
+    }
+}
+export default FormulaEvaluator;
+
---
+++ src/Tests/Unit/NewFormulaEvaluator.test.ts
@@ -0,0 +1,511 @@
+// Unit tests for Recalc.ts
+//
+// Path: src/Tests/Unit/Recalc.test.ts
+// Compare this snippet from src/Engine/Recalc.ts:
+//
+// // Recalc.ts
+// export class Recalc {
+//   private machine: Machine;
+//   private errorOccured: boolean = false;
+
+import FormulaEvaluator from "../../Engine/NewFormulaEvaluator";
+import SheetMemory from "../../Engine/SheetMemory";
+import Cell from "../../Engine/Cell";
+import { ErrorMessages } from "../../Engine/GlobalDefinitions";
+
+
+let testMemory: SheetMemory;
+let recalc: FormulaEvaluator;
+
+
+beforeEach(() => {
+    testMemory = new SheetMemory(5, 5);
+    recalc = new FormulaEvaluator(testMemory);
+
+
+    const cellA1 = new Cell();
+    cellA1.setFormula(["1"]);
+    cellA1.setValue(1);
+    cellA1.setError("");
+    testMemory.setWorkingCellByCoordinates(0, 0);
+    testMemory.setCurrentCell(cellA1);
+
+    const cellA2 = new Cell();
+    cellA2.setFormula(["2"]);
+    cellA2.setValue(2);
+    cellA2.setError("");
+    testMemory.setWorkingCellByCoordinates(0, 1);
+    testMemory.setCurrentCell(cellA2);
+
+    const cellA3 = new Cell();
+    cellA3.setFormula(["3"]);
+    cellA3.setValue(3);
+    cellA3.setError("");
+    testMemory.setWorkingCellByCoordinates(0, 2);
+    testMemory.setCurrentCell(cellA3);
+});
+
+describe("FormulaEvaluator", () => {
+    describe("update", () => {
+        describe("when the contains a single number", () => {
+            it("returns the number", () => {
+                const formula: FormulaType = ["1"];
+                const memory = new SheetMemory(5, 5);
+
+                recalc.evaluate(formula)
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(1);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is ( 8 )", () => {
+            it("returns the number", () => {
+                const formula: FormulaType = ["(", "8", ")"];
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(8);
+                expect(error).toEqual("");
+            });
+        });
+
+
+        describe("when the formula contains two tokens, number, operator", () => {
+            it("returns the number", () => {
+                const formula: FormulaType = ["1", "+"];
+
+                recalc.evaluate(formula);
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(1);
+                expect(error).toEqual(ErrorMessages.invalidFormula);
+            });
+        });
+
+        describe("when the formula contains three tokens, number, operator, number", () => {
+            describe("when the operator is +", () => {
+                it("returns the sum of the numbers", () => {
+                    const formula: FormulaType = ["1", "+", "2"];
+                    const memory = new SheetMemory(5, 5);
+                    recalc.evaluate(formula);
+
+                    let result = recalc.result;
+                    let error = recalc.error;
+
+                    expect(result).toEqual(3);
+                    expect(error).toEqual("");
+                });
+            });
+
+            describe("when the operator is -", () => {
+                it("returns the difference of the numbers", () => {
+                    const formula: FormulaType = ["1", "-", "2"];
+                    const memory = new SheetMemory(5, 5);
+                    recalc.evaluate(formula);
+
+                    let result = recalc.result;
+                    let error = recalc.error;
+
+                    expect(result).toEqual(-1);
+                    expect(error).toEqual("");
+                });
+            });
+
+
+
+            describe("when the operator is *", () => {
+                it("returns the product of the numbers", () => {
+                    const formula: FormulaType = ["1", "*", "2"];
+                    const memory = new SheetMemory(5, 5);
+                    recalc.evaluate(formula);
+
+                    let result = recalc.result;
+                    let error = recalc.error;
+
+                    expect(result).toEqual(2);
+                    expect(error).toEqual("");
+                });
+            });
+
+
+            describe("when the operator is /", () => {
+                describe("when the divisor is not zero", () => {
+                    it("returns the quotient of the numbers", () => {
+                        const formula: FormulaType = ["1", "/", "2"];
+
+                        recalc.evaluate(formula);
+
+                        let result = recalc.result;
+                        let error = recalc.error;
+
+                        expect(result).toEqual(0.5);
+                        expect(error).toEqual("");
+                    });
+                });
+
+                describe("when the divisor is zero", () => {
+                    it("returns an error", () => {
+                        const formula: FormulaType = ["1", "/", "0"];
+
+                        recalc.evaluate(formula);
+
+                        let result = recalc.result;
+                        let error = recalc.error;
+
+                        expect(result).toEqual(Infinity);
+                        expect(error).toEqual("#DIV/0!");
+                    });
+                });
+            });
+        });
+
+        describe(" The formula is ( )", () => {
+            it("returns a syntax error", () => {
+                const formula: FormulaType = ["(", ")"];
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(0);
+                expect(error).toEqual(ErrorMessages.missingParentheses);
+            });
+        });
+
+        describe("when the formula contains 8 ( ", () => {
+            it("returns a syntax error", () => {
+                const formula: FormulaType = ["8", "("];
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+
+                expect(result).toEqual(8);
+                expect(error).toEqual(ErrorMessages.invalidFormula);
+            });
+        });
+
+        describe("when the formula contains five tokens, number, operator, number, operator, number", () => {
+            describe("when the operators are +, +", () => {
+                it("returns the sum of all three numbers", () => {
+                    const formula: FormulaType = ["1", "+", "2", "+", "3"];
+
+                    recalc.evaluate(formula);
+
+                    let result = recalc.result;
+                    let error = recalc.error;
+                    expect(result).toEqual(6);
+                    expect(error).toEqual("");
+                });
+            });
+
+            describe("when the operators are +, -", () => {
+                it("returns the sum of the first two numbers minus the third number", () => {
+                    const formula: FormulaType = ["1", "+", "2", "-", "3"];
+                    recalc.evaluate(formula);
+
+                    let result = recalc.result;
+                    let error = recalc.error;
+
+                    expect(result).toEqual(0);
+                    expect(error).toEqual("");
+                });
+            });
+
+            describe("when the operators are +, *", () => {
+                it("returns the product of the second and third number added to the first number", () => {
+                    const formula: FormulaType = ["1", "+", "2", "*", "3"];
+                    recalc.evaluate(formula);
+
+                    let result = recalc.result;
+                    let error = recalc.error;
+
+                    expect(result).toEqual(7);
+                    expect(error).toEqual("");
+
+                });
+            });
+
+
+            describe("when the operators are +, /", () => {
+                it("returns the quotient of the second and third number added to the first number", () => {
+
+                    const formula: FormulaType = ["1", "+", "10", "/", "5"];
+                    recalc.evaluate(formula);
+
+                    let result = recalc.result;
+                    let error = recalc.error;
+
+                    expect(result).toEqual(3);
+                    expect(error).toEqual("");
+
+                });
+            });
+        });
+
+        describe("when the formula contains four tokens, number, operator, number, operator", () => {
+            it("returns the result of the first three tokens", () => {
+                const formula: FormulaType = ["1", "+", "2", "+"];
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(3);
+                expect(error).toEqual(ErrorMessages.invalidFormula);
+            }
+            );
+        }
+        );
+        describe("when the formula A1 + A1", () => {
+
+            it("returns the number", () => {
+                const formula = ["A1", "+", "A1"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+                expect(result).toEqual(2);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula A1 + A2", () => {
+            it("returns the number", () => {
+                const formula = ["A1", "+", "A2"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(3);
+                expect(error).toEqual("");
+
+            });
+        });
+
+        describe("when the formula A1 + A2 + 50", () => {
+
+            it("returns the number", () => {
+
+
+                const formula = ["A1", "+", "A2", "+", "50"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(53);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is 1 * ) ", () => {
+            it("returns the number", () => {
+                const formula = ["1", "+", "+"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(1);
+                expect(error).toEqual(ErrorMessages.invalidFormula);
+            });
+        });
+
+        describe("when the formula is - 1", () => {
+            it("returns the number -1", () => {
+                const formula = ["-", "1"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(-1);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is 1 '+/-' ", () => {
+            it("returns the number", () => {
+                const formula = ["1", "+/-"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(-1);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is 3.14159265358979323  cos' ", () => {
+            it("returns the numbe 0", () => {
+                const formula = ["3.14159265358979323", "cos"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(-1);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is 3.14159265358979323  sin' ", () => {
+            it("returns the numbe 0", () => {
+                const formula = ["3.14159265358979323", "sin"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(Math.abs(result)).toBeLessThan(0.000000000000001);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is (3.14159265358979323 / 4)  tan' ", () => {
+            it("returns the numbe 0", () => {
+                const formula = ["(", "3.14159265358979323", "/", "4", ")", "tan"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+                let delta = Math.abs(result - 1);
+                expect(delta).toBeLessThan(0.000000000000001);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is 1  asin' ", () => {
+            it("returns the number PI/2", () => {
+                const formula = ["1", "asin"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(1.5707963267948966);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is 1  acos' ", () => {
+            it("returns the numbe 0", () => {
+                const formula = ["1", "acos"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(0);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is 1  atan' ", () => {
+            it("returns the number PI/4", () => {
+                const formula = ["1", "atan"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(0.7853981633974483);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is 4  sqrt' ", () => {
+            it("returns the number 2", () => {
+                const formula = ["4", "sqrt"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(2);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is 8  cuberoot' ", () => {
+            it("returns the number 2", () => {
+                const formula = ["8", "cuberoot"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(2);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is 4  sqr' ", () => {
+            it("returns the number 16", () => {
+                const formula = ["4", "sqr"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(16);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is 4  cube' ", () => {
+            it("returns the number 64", () => {
+                const formula = ["4", "cube"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(64);
+                expect(error).toEqual("");
+            });
+        });
+
+        describe("when the formula is 4  1/x' ", () => {
+            it("returns the number 0.25", () => {
+                const formula = ["4", "1/x"];
+
+                recalc.evaluate(formula);
+
+                let result = recalc.result;
+                let error = recalc.error;
+
+                expect(result).toEqual(0.25);
+                expect(error).toEqual("");
+            });
+        });
+
+
+    });
+});
