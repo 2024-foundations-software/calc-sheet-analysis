50b515a0041572a3c97f05761bb09a8d809568f2
sillyfunnypedro <sillyfunnypedro@gmail.com>
2023-10-17 13:11:01 -0700

    user controll pulled in

--- src/Tests/Integration/MachineSimple.test.ts
+++ src/Tests/Integration/MachineSimple.test.ts
@@ -23,21 +23,26 @@ describe("SpreadSheetController", () => {
   describe("accessControl", () => {
     it("should true if the user asks for access to a cell in the empty sheet", () => {
       const machine = new SpreadSheetController(5, 5);
+      const view = machine.requestViewAccess("user1", "A1");
       const result = machine.requestEditAccess("user1", "A1");
       expect(result).toEqual(true);
     });
 
     it("should false if the user asks for access to a cell that another user owns", () => {
       const machine = new SpreadSheetController(5, 5);
+      let view = machine.requestViewAccess("user1", "A1");
       const result = machine.requestEditAccess("user1", "A1");
+      view = machine.requestViewAccess("user2", "A1");
       const result2 = machine.requestEditAccess("user2", "A1");
       expect(result2).toEqual(false);
     });
 
     it("should return true if the user has released access and another user asks for access", () => {
       const machine = new SpreadSheetController(5, 5);
+      let view = machine.requestViewAccess("user1", "A1");
       const result = machine.requestEditAccess("user1", "A1");
       const result2 = machine.releaseEditAccess("user1");
+      view = machine.requestViewAccess("user2", "A1");
       const result3 = machine.requestEditAccess("user2", "A1");
       expect(result3).toEqual(true);
     });
@@ -46,11 +51,13 @@ describe("SpreadSheetController", () => {
   describe("editing the sheet", () => {
     it("should return two formulas for two users editing a sheet", () => {
       const machine = new SpreadSheetController(5, 5);
+      let view = machine.requestViewAccess("user1", "A1");
       machine.requestEditAccess("user1", "A1");
       machine.addToken("1", "user1");
       machine.addToken("+", "user1");
       machine.addToken("7", "user1");
 
+      view = machine.requestViewAccess("user2", "A2");
       machine.requestEditAccess("user2", "A2");
       machine.addToken("2", "user2");
       machine.addToken("+", "user2");
@@ -67,11 +74,13 @@ describe("SpreadSheetController", () => {
 
     it("should ignore requests to update cell from non user", () => {
       const machine = new SpreadSheetController(5, 5);
+      machine.requestViewAccess("user1", "A1");
       machine.requestEditAccess("user1", "A1");
       machine.addToken("1", "user1");
       machine.addToken("+", "user1");
       machine.addToken("7", "user1");
 
+      machine.requestViewAccess("user2", "A2");
       machine.requestEditAccess("user2", "A1");
       machine.addToken("2", "user2");
       machine.addToken("+", "user2");
@@ -89,11 +98,13 @@ describe("SpreadSheetController", () => {
 
     it("should add a cell to the user formula", () => {
       const machine = new SpreadSheetController(5, 5);
+      let view = machine.requestViewAccess("user1", "A1");
       machine.requestEditAccess("user1", "A1");
       machine.addToken("1", "user1");
       machine.addToken("+", "user1");
       machine.addToken("7", "user1");
 
+      view = machine.requestViewAccess("user2", "A2");
       machine.requestEditAccess("user2", "A2");
       machine.addCell("A1", "user2");
 
@@ -109,11 +120,14 @@ describe("SpreadSheetController", () => {
 
     it("should not add a cell that could produce a circular reference", () => {
       const machine = new SpreadSheetController(5, 5);
+      machine.requestViewAccess("user1", "A1");
       machine.requestEditAccess("user1", "A1");
       machine.addToken("1", "user1");
       machine.addToken("+", "user1");
       machine.addCell("A2", "user1");
 
+      machine.requestViewAccess("user2", "A2");
+      const accessControl = machine.requestEditAccess("user2", "A2");
       machine.requestEditAccess("user2", "A2");
       machine.addToken("1", "user2");
       machine.addToken("+", "user2");
@@ -131,11 +145,13 @@ describe("SpreadSheetController", () => {
 
     it("should not add a cell after the offending cell is removed.", () => {
       const machine = new SpreadSheetController(5, 5);
+      let view = machine.requestViewAccess("user1", "A1");
       machine.requestEditAccess("user1", "A1");
       machine.addToken("1", "user1");
       machine.addToken("+", "user1");
       machine.addCell("A2", "user1");
 
+      view = machine.requestViewAccess("user2", "A2");
       machine.requestEditAccess("user2", "A2");
       machine.addToken("1", "user2");
       machine.addToken("+", "user2");
@@ -152,11 +168,13 @@ describe("SpreadSheetController", () => {
 
       machine.removeToken("user1");
       machine.addCell("B3", "user1");
+      view = machine.requestViewAccess("user1", "B3");
       machine.requestEditAccess("user1", "B3");
       machine.addToken("1", "user1");
       machine.addToken("7", "user1");
 
       machine.addCell("A1", "user2");
+      view = machine.requestViewAccess("user1", "A1");
       machine.requestEditAccess("user1", "A1");
       user1Formula = machine.getFormulaStringForUser("user1");
       user2Formula = machine.getFormulaStringForUser("user2");
